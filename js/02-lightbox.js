627e12f29fb160ae49ad20d0ca963cbf08f4b560
sandra9ermak <sandra1ermak@gmail.com>
2021-09-28 17:35:23 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryAddItems = galleryItems.map(item => {
+  return `<a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt="${item.description}" />
+    </a>`;
+})
+
+const gallJoin = galleryAddItems.join('');
+gallery.insertAdjacentHTML('afterbegin', gallJoin);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250
+});
